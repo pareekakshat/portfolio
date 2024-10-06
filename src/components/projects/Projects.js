6637853dca18a9d2bb20bd72097e861a51f4e9c1
pareekakshat <akshatpareek44@gmail.com>
2024-10-06 22:36:09 +0530

    updates

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -24,39 +24,39 @@ const Projects = () => {
           title="Blog-Wise"
           des="A blogging Full-Stack Web Application with features such as regular email/password and Google OAuth Sign-In options, CSRF attack prevention, theme switching, email verification, and password reset."
           src={projectOne}
-          githubLink="https://github.com/coderrohan14/BlogWise"
-          websiteLink="https://coderrohan14.github.io/BlogWise-Client/"
+          githubLink="https://github.com"
+          websiteLink=""
         />
         <ProjectsCard
           title="ECommerce-AWS-Serverless"
           des="AWS Serverless E-Commerce app built using AWS CDK and AWS SDK incorporating event-driven microservices architecture."
           src={projectTwo}
-          githubLink="https://github.com/coderrohan14/ECommerce-AWS-Serverless"
+          githubLink="https://github.com"
         />
         <ProjectsCard
           title="Jobs-Application-Management-API"
           des="A REST API built with Node.js, Express.js, and Mongoose, integrating Swagger UI documentation using swagger-ui-express and yamljs."
           src={projectThree}
-          githubLink="https://github.com/coderrohan14/Jobs-Application-Management-API"
-          websiteLink="https://sore-red-chipmunk-tam.cyclic.app/api-docs/"
+          githubLink="https://github.com"
+          websiteLink=""
         />
         <ProjectsCard
           title="Rail-Companion"
           des="A real-time cloud-update application with QR code-based verification, using Android SDK for developing the application, along with Firebase Firestore Database, CameraX library, and Jetpack Compose."
           src={projectFour}
-          githubLink="https://github.com/coderrohan14/RailCompanion"
+          githubLink="https://github.com"
         />
         <ProjectsCard
           title="Covid-Zap"
           des="An Android application which helped users check the availability of beds in nearby hospitals along with the distance of the hospital from the user’s location during the Covid-19 pandemic, implemented using Covid-19 REST API, Volley, Firebase Auth, Google Maps API, and Geo-coder API."
           src={projectFive}
-          githubLink="https://github.com/coderrohan14/CovidZap"
+          githubLink="https://github.com"
         />
         <ProjectsCard
           title="Shopping-Helper"
           des="An Android shopping list application implemented using Recycler View and Live Data using MVVM architecture where the users can store the shopping items along with their quantity and can also edit them later."
           src={projectSix}
-          githubLink="https://github.com/coderrohan14/ShoppingHelper"
+          githubLink="https://github.com"
         />
       </div>
     </section>
